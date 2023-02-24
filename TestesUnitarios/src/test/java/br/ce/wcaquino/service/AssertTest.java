9d1111fe9499b9a0dc2fda7182721f822b543dd6
PauloEd-hub <pauloedu.cavalcante@gmail.com>
2023-02-24 11:35:04 -0300

    Improving tests using AssertThat

--- TestesUnitarios/src/test/java/br/ce/wcaquino/service/AssertTest.java
+++ TestesUnitarios/src/test/java/br/ce/wcaquino/service/AssertTest.java
@@ -13,9 +13,10 @@ public class AssertTest {
 		Assert.assertFalse(false);
 		
 		
-		Assert.assertEquals(1, 1);
+		Assert.assertEquals("Erro de comparação",1, 1);
 		Assert.assertEquals(0.51, 0.51, 0.01);
 		Assert.assertEquals(0.51234, 0.512, 0.001);
+		Assert.assertEquals(Math.PI, 3.14, 0.01);
 		
 		
 		int i = 5;//Tipo primitivo
@@ -24,16 +25,21 @@ public class AssertTest {
 		Assert.assertEquals(Integer.valueOf(i), i2);
 		Assert.assertEquals(i, i2.intValue());
 		
+		
+		
+		Assert.assertEquals("bola", "bola");
+		Assert.assertNotEquals("bola", "casa");
 		Assert.assertTrue("bola".equalsIgnoreCase("Bola"));
 		Assert.assertTrue("bola".startsWith("bo"));
 		
 		Usuario u1 = new  Usuario("Usuario 1");
 		Usuario u2 = new Usuario("Usuario 1");
-		Usuario u3 = u2;
+		Usuario u3 = null;
 
 		Assert.assertEquals(u1, u2);
-		
-		Assert.assertSame(u1, u2);
+		Assert.assertSame(u2, u2);
+		Assert.assertTrue(u3 == null);
+	
 	}
 
 }
--- TestesUnitarios/src/test/java/br/ce/wcaquino/service/LocacaoServiceTest.java
+++ TestesUnitarios/src/test/java/br/ce/wcaquino/service/LocacaoServiceTest.java
@@ -1,8 +1,12 @@
 package br.ce.wcaquino.service;
 
 
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
 import java.util.Date;
 
+import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -28,10 +32,20 @@ public class LocacaoServiceTest {
 		Locacao locacao = service.alugarFilme(usuario, filme);
 		
 		
-		//verificacao
-		Assert.assertTrue(locacao.getValor() == 5.0);
-		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
-		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+		//verificacao Assert
+//		Assert.assertEquals(locacao.getValor(), 5.0, 0.1);
+//		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
+//		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+		
+		
+		
+		
+		//Verifcação AssertThat
+		assertThat(null, null);
+		assertThat(locacao.getValor(),CoreMatchers.is(CoreMatchers.equalTo(5.0)));
+		assertThat(locacao.getValor(),CoreMatchers.is(CoreMatchers.not(6.0)));
+		assertThat(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()), CoreMatchers.is(true));
+		assertThat(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)), CoreMatchers.is(true));
 		
 	}
 
